64b6ae9b2aacf87c64a9890e7a92fcfc4c032de2
Rajiv Singaseni <rajiv@webileapps.com>
2021-02-10 15:45:21 +0530

    Add bpastateholder-citizen to the remote routes config.

--- web/rainmaker/packages/employee/src/ui-config/commonConfig/remote-config-paths/index.js
+++ web/rainmaker/packages/employee/src/ui-config/commonConfig/remote-config-paths/index.js
@@ -21,6 +21,8 @@ const remoteConfigPath = (path, screenKey) => {
       break;
     case "egov-bpa":
     case "oc-bpa":
+    case "bpastakeholder-citizen":
+    case "bpastakeholder":
       config = require(`egov-bpa/ui-config/screens/specs/${path}/${screenKey}`).default;
       break;
     case "fire-noc":

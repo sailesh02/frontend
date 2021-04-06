8803f42fe0ea81352898c5f307ffda819902594f
od-egov-git <74169069+od-egov-git@users.noreply.github.com>
2021-04-06 06:40:55 +0000

    Update actions.js

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/redux/auth/actions.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/redux/auth/actions.js
@@ -139,6 +139,7 @@ export const logout = () => {
                 // For Finance
         if(process.env.REACT_APP_NAME !== "Citizen"){
           const response_finance = await httpRequest(FINANCE.LOGOUT.URL, FINANCE.LOGOUT.ACTION, [{ key: "access_token", value: authToken }]);
+        }
       } else {
         clearUserDetails();
         process.env.REACT_APP_NAME === "Citizen"

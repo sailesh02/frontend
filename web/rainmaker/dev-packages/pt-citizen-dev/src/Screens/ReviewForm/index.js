9b104b3e0b27d61eec8760f9599448eb039b9de5
Tejaswi Katha <tejaswi.k@transerve.com>
2021-05-07 13:50:03 +0530

    assessment get bill and fetch bill and acknowledgement page

--- web/rainmaker/dev-packages/pt-citizen-dev/src/Screens/ReviewForm/index.js
+++ web/rainmaker/dev-packages/pt-citizen-dev/src/Screens/ReviewForm/index.js
@@ -41,7 +41,7 @@ class ReviewForm extends Component {
 
   componentDidMount() {
     this.getImportantDates();
-    this.props.getEstimates();
+    // this.props.getEstimates();
   }
 
   getImportantDates = async () => {
@@ -238,12 +238,12 @@ class ReviewForm extends Component {
                 />
 
               </div>
-              {formWizardConstants[purpose].isEstimateDetails && <PropertyTaxDetailsCard
+              {/* {formWizardConstants[purpose].isEstimateDetails && <PropertyTaxDetailsCard
                 estimationDetails={estimationDetails}
                 importantDates={importantDates}
                 openCalculationDetails={this.openCalculationDetails}
                 optionSelected={valueSelected}
-              />}
+              />} */}
               <PropertyAddressInfo OldProperty={OldProperty} generalMDMSDataById={generalMDMSDataById} properties={this.props.properties} editIcon={formWizardConstants[purpose].isEditButton ? <EditIcon onIconClick={() => onEditButtonClick(0)} /> : null}></PropertyAddressInfo>
               <AssessmentInfo OldProperty={OldProperty} generalMDMSDataById={generalMDMSDataById} properties={this.props.properties} editIcon={formWizardConstants[purpose].isEditButton ? <EditIcon onIconClick={() => onEditButtonClick(1)} /> : null}></AssessmentInfo>
               <OwnerInfo OldProperty={OldProperty} generalMDMSDataById={generalMDMSDataById} properties={this.props.properties} editIcon={formWizardConstants[purpose].canEditOwner ? <EditIcon onIconClick={() => onEditButtonClick(2)} /> : null}></OwnerInfo>

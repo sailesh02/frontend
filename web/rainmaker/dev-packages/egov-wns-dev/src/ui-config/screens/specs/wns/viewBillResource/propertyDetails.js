c8c3cf27e6f15733e42748011320c915e9fce342
Ruchira Naik <ruchira.naik@transerve.com>
2021-05-31 13:25:15 +0530

    fixed issue: few values coming as NA in view details section in citizen

--- web/rainmaker/dev-packages/egov-wns-dev/src/ui-config/screens/specs/wns/viewBillResource/propertyDetails.js
+++ web/rainmaker/dev-packages/egov-wns-dev/src/ui-config/screens/specs/wns/viewBillResource/propertyDetails.js
@@ -32,49 +32,49 @@ export const getProperty = () => {
                     labelKey: "WS_PROP_DETAIL_CITY"
                 },
                 {
-                    jsonPath: "WaterConnection[0].property.address.city"
+                    jsonPath: "WaterConnection[0].tenantId"
                 }
             ),
-            propertyDoorNo: getLabelWithValue(
-                {
-                    labelKey: "WS_PROP_DETAIL_PHNO_LABEL"
-                },
-                {
-                    jsonPath: "WaterConnection[0].property.address.doorNo"
-                }
-            ),
-            propertyBuilding: getLabelWithValue(
-                {
-                    labelKey: "WS_PROP_DETAIL_BUILD_NAME_LABEL"
-                },
-                {
-                    jsonPath: "WaterConnection[0].property.address.buildingName"
-                }
-            ),
-            propertyStreet: getLabelWithValue(
-                {
-                    labelKey: "WS_PROP_DETAIL_STREET_NAME"
-                },
-                {
-                    jsonPath: "WaterConnection[0].property.address.street"
-                }
-            ),
+            // propertyDoorNo: getLabelWithValue(
+            //     {
+            //         labelKey: "WS_PROP_DETAIL_PHNO_LABEL"
+            //     },
+            //     {
+            //         jsonPath: "WaterConnection[0].property.address.doorNo"
+            //     }
+            // ),
+            // propertyBuilding: getLabelWithValue(
+            //     {
+            //         labelKey: "WS_PROP_DETAIL_BUILD_NAME_LABEL"
+            //     },
+            //     {
+            //         jsonPath: "WaterConnection[0].property.address.buildingName"
+            //     }
+            // ),
+            // propertyStreet: getLabelWithValue(
+            //     {
+            //         labelKey: "WS_PROP_DETAIL_STREET_NAME"
+            //     },
+            //     {
+            //         jsonPath: "WaterConnection[0].property.address.street"
+            //     }
+            // ),
             propertyMohalla: getLabelWithValue(
                 {
                     labelKey: "WS_PROP_DETAIL_LOCALITY_MOHALLA_LABEL"
                 },
                 {
-                    jsonPath: "WaterConnection[0].property.address.locality.name"
+                    jsonPath: "WaterConnection[0].additionalDetails.locality"
                 }
             ),
-            propertyPincode: getLabelWithValue(
-                {
-                    labelKey: "WS_PROP_DETAIL_PINCODE"
-                },
-                {
-                    jsonPath: "WaterConnection[0].property.address.pincode"
-                }
-            )
+            // propertyPincode: getLabelWithValue(
+            //     {
+            //         labelKey: "WS_PROP_DETAIL_PINCODE"
+            //     },
+            //     {
+            //         jsonPath: "WaterConnection[0].property.address.pincode"
+            //     }
+            // )
         })
     })
 };

8b9dcd97cdd0f9a39310e31adc4fb77c13d3f17e
neeraj <kumar.neeraj@velocis.co.in>
2021-08-23 13:39:57 +0530

    refactor: by pass language selection page due slow page

--- web/rainmaker/packages/citizen/src/modules/citizen/User/Login/index.js
+++ web/rainmaker/packages/citizen/src/modules/citizen/User/Login/index.js
@@ -3,18 +3,39 @@ import formHoc from "egov-ui-kit/hocs/form";
 import LoginForm from "./components/LoginForm";
 import { Banner } from "modules/common";
 import { connect } from "react-redux";
+import { getLocale, getTenantId, setTenantId, getUserInfo } from "egov-ui-kit/utils/localStorageUtils";
 import get from "lodash/get";
+import { fetchLocalizationLabel } from "egov-ui-kit/redux/app/actions";
+import { getQueryArg } from "egov-ui-kit/utils/commons";
 
 const LoginFormHOC = formHoc({ formKey: "login" })(LoginForm);
 
 class Login extends Component {
   //className="col-lg-offset-2 col-md-offset-2 col-md-8 col-lg-8"
+  state = {
+    languageSelected: getLocale(),
+  }
+
+  onLanguageChange = (event, index, value) => {
+    //const {setRote} = this.props;
+    this.setState({ languageSelected: value });
+    let tenantId = getTenantId();
+
+    if (process.env.REACT_APP_NAME === "Citizen") {
+      const tenantInfo=getQueryArg(window.location.href, "tenantId")
+      const userInfo = JSON.parse(getUserInfo());
+      tenantId = userInfo && userInfo.permanentCity;
+      tenantId = tenantInfo?tenantInfo:tenantId;
+    }
+    this.props.fetchLocalizationLabel(value, tenantId, tenantId);
+  };
   render() {
-    const { bannerUrl, logoUrl,qrCodeURL,enableWhatsApp } = this.props;
+    const { bannerUrl, logoUrl,qrCodeURL,enableWhatsApp, languages,hasLocalisation } = this.props;
 
     return (
-      <Banner bannerUrl={bannerUrl} logoUrl={logoUrl}>
-        <LoginFormHOC logoUrl={logoUrl} qrCodeURL={qrCodeURL} enableWhatsApp={enableWhatsApp}/>
+      // <Banner bannerUrl={bannerUrl} logoUrl={logoUrl}>
+      <Banner logoUrl={logoUrl}>
+        <LoginFormHOC logoUrl={logoUrl} qrCodeURL={qrCodeURL} enableWhatsApp={enableWhatsApp}languages ={languages} onLanguageChange={this.onLanguageChange} hasLocalisation={hasLocalisation} languageSelected={this.state.languageSelected}/>
       </Banner>
     );
   }
@@ -25,11 +46,22 @@ const mapStateToProps = ({ common }) => {
   let bannerUrl = get(stateInfoById, "0.bannerUrl");
   let logoUrl = get(stateInfoById, "0.logoUrl");
   let qrCodeURL = get(stateInfoById, "0.qrCodeURL");
-  let enableWhatsApp=get(stateInfoById,"0.enableWhatsApp"); 
-  return { bannerUrl, logoUrl,qrCodeURL ,enableWhatsApp};
+  let enableWhatsApp=get(stateInfoById,"0.enableWhatsApp");
+  let languages = get(stateInfoById, "0.languages", []);
+  let hasLocalisation;
+  if (stateInfoById && stateInfoById.length > 0) {
+   hasLocalisation = stateInfoById[0].hasLocalisation;
+  }
+  return { bannerUrl, logoUrl,qrCodeURL ,enableWhatsApp,languages , hasLocalisation};
+};
+
+const dispatchToProps = dispatch => {
+  return {
+    fetchLocalizationLabel: (locale, tenantId,tenant) => dispatch(fetchLocalizationLabel(locale, tenantId, tenant))
+  };
 };
 
 export default connect(
   mapStateToProps,
-  null
+  dispatchToProps
 )(Login);

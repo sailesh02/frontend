217bc01d01d36b0bb571652ff9bbdfb651e9711c
Ruchira Naik <ruchira.naik@transerve.com>
2021-08-23 10:00:32 +0530

    reverted changes

--- web/rainmaker/dev-packages/egov-bpa-dev/src/ui-molecules-local/Table/index.js
+++ web/rainmaker/dev-packages/egov-bpa-dev/src/ui-molecules-local/Table/index.js
@@ -30,8 +30,8 @@ class Table extends React.Component {
             fontSize: 14
           },
           head: {
-            color: "rgba(0, 0, 0, 0.8700000048)",
-            fontWeight: 700,
+            color: this.props.isApprover ? "#7F7E7A" : "rgba(0, 0, 0, 0.8700000048)",
+            fontWeight: this.props.isApprover ? 400 : 700,
             fontSize: 14
           }
         },

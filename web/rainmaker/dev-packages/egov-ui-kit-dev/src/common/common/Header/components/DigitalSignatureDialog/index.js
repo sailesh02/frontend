5a94ce8d189f85b40bd7ea79d08813dd5b06ac88
Ruchira Naik <ruchira.naik@transerve.com>
2021-09-01 14:54:02 +0530

    added function to getToken list

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/common/Header/components/DigitalSignatureDialog/index.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/common/Header/components/DigitalSignatureDialog/index.js
@@ -21,7 +21,21 @@ import {
 import TextField from "material-ui/TextField";
 import { getLocale, getTenantId,getAccessToken, getUserInfo } from "egov-ui-kit/utils/localStorageUtils";
 import axios from 'axios';
-
+import { toggleSpinner } from "egov-ui-framework/ui-redux/screen-configuration/actions";
+import {getTokenList} from './functions'
+const authToken = getAccessToken();
+let RequestInfo = {
+  apiId: "Rainmaker",
+  ver: ".01",
+  // ts: getDateInEpoch(),
+  action: "_search",
+  did: "1",
+  key: "",
+  msgId: `20170310130900|${getLocale()}`,
+  requesterId: "",
+  authToken
+};
+let customRequestInfo = JSON.parse(getUserInfo())
 class DigitalSignatureDialog extends Component {
   state = {
     tokensArray : '',
@@ -51,91 +65,96 @@ class DigitalSignatureDialog extends Component {
   }
 
   getTokenList = () => {
-    const authToken = getAccessToken();
-    let RequestInfo = {
-      apiId: "Rainmaker",
-      ver: ".01",
-      // ts: getDateInEpoch(),
-      action: "_search",
-      did: "1",
-      key: "",
-      msgId: `20170310130900|${getLocale()}`,
-      requesterId: "",
-      authToken
-    };
-  
-    RequestInfo = { ...RequestInfo };
+    let tokenList = []
+    store.dispatch(toggleSpinner());
+    RequestInfo = { ...RequestInfo,"userInfo" :customRequestInfo};
     let body =  Object.assign(
       {},
       {
-        RequestInfo
+        RequestInfo,
+        "tenantId":getTenantId(),
+        "responseData":null, responseData:null
       }
-    
     );
-
+    
     axios.post("/DSC/getR1R2", body, { // to get R1 R2
       'Content-Type': 'application/json',
       'Accept': 'application/json'
      })
       .then(response => {
-        if(response){
-          let data = response.data.info
-          let body =  Object.assign(
-            {},
-            {
-              RequestInfo
-            },
-            {
-              data
-            }
-          
-          );
-          axios.post("/DSC/getR1R2", body, { //to get access token
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-           })
-            .then(response => {
-              let data = response.data.responseData
-              let body =  Object.assign(
-                {},
-                {
-                  RequestInfo
-                },
-                {
-                  data
-                }
-              
-              );
-              if(response){
-                axios.post("/DSC/getR1R2", body, { //to get list of tokens
-                  'Content-Type': 'application/json',
-                  'Accept': 'application/json'
-                 })
-                  .then(response => {
-                    if(response){
-                      this.setState({
-                        tokensArray : response.data.data
-                      })
-                    }
-                  })
-                  .catch(error => {
-                    console.log(error.response)
-                  });
-              }
-            })
-            .catch(error => {
-              console.log(error.response)
-            });
-        }
+        // response.data.input
       })
       .catch(error => {
-        console.log(error.response)
+        let response = {
+          "ResponseInfo":null,
+          "input":{
+          "encryptedRequest":"XXXX",
+              "encryptionKeyId":"YYYYY"
+          }
+        } 
+        let body = response.input
+        axios.post("/.emudhra.com:26769/DSC/ListToken", body, { // to get R1 R2
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+         })
+          .then(response => {
+            // response.responseData
+          })
+          .catch(error => {
+            let response = {
+              "responseData": "QuyKW0m6EdBEuTltdWrj2rA5O77bukrGcxlpp4atnn0KoadBXlTZXoEpHp3Q3Qxne1pcmXUSBedS3Ocj3/5Nqjtj6Q1QuqQxo1yMtoOmeGjlilICxaqs9ldgRu8rlbuXrzeip6VMqMCEM+f21+tKf3c4UKWd6/gYOg8rG+37HAVDRAjz21HECLLP2lq3bThBTIPog74D8Lvs4MHXE7D28kd2znHny2v/r3lGnmLxmzYlMiBUlYPnPQa8WSyOROpfXNDnD0/fgiIUuNA82mXC7F7x4VHf+GYj94aldkeSE7MKSqDPRsSp3/4gJ4Y8bHNa",
+              "status": 1,
+              "errorMessage": null,
+              "version": "3.1.0.0",
+              "errorCode": null
+             }
+
+             RequestInfo = { ...RequestInfo,"userInfo" :customRequestInfo};
+             let body =  Object.assign(
+               {},
+               {
+                 RequestInfo,
+                 "tenantId":getTenantId(),
+                 "responseData":null, responseData:response.responseData
+               }
+             
+             );
+            axios.post("/dsc-services/dsc/_getTokens", body, { // to get R1 R2
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'
+             })
+              .then(response => {
+                // response.responseData
+              })
+              .catch(error => { //will return tokens
+                let response = {
+                  "ResponseInfo":null,
+                  "tokens":[
+                  "Token 1",
+                      "Token 2"
+                  ]
+                  } 
+                let requiredTokenFormat = response.tokens.map (token => {
+                  return {
+                    label : token,
+                    value : token
+                  }
+                }) 
+                tokenList = requiredTokenFormat
+                this.setState({
+                  tokensArray : requiredTokenFormat,
+                  selectedToken : requiredTokenFormat[0].label
+                })
+                store.dispatch(toggleSpinner());  
+              });
+          });
       });
+      return tokenList
   
   }
 
   componentDidMount = () => {
-    this.getTokenList()
+     this.getTokenList()
   }
 
   render() {
@@ -201,11 +220,11 @@ class DigitalSignatureDialog extends Component {
                             labelName: "Select Token",
                             labelKey: "CORE_COMMON_SELECT_TOKEN_LABEL"
                           }}
-                        data={[]}
+                        data={this.state.tokensArray}
                         optionValue="value"
                         optionLabel="label"
                         hasLocalization={false}
-                        value = {"ksjk"}
+                        value = {this.state.selectedToken}
                       />
                   </Grid>
                   <Grid

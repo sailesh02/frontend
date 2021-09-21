cc1d1285235be4392f2e6788444765723df05d11
Ruchira Naik <ruchira.naik@transerve.com>
2021-09-21 10:39:18 +0530

    removed unused code

--- web/rainmaker/dev-packages/egov-workflow-dev/src/ui-molecules-local/Footer/index.js
+++ web/rainmaker/dev-packages/egov-workflow-dev/src/ui-molecules-local/Footer/index.js
@@ -85,123 +85,6 @@ let customRequestInfo = JSON.parse(getUserInfo())
         return data.tenantId  
     }
   }
-  // for testing using catch block
-  // export const getPdfDetails = async (data,preparedFinalObject,moduleName) => {
-  //     let {DsInfo} = preparedFinalObject
-  //     let {token,certificate,password} = DsInfo
-  //     let body = getPdfBody(moduleName,preparedFinalObject)
-  //     let key = getKey(moduleName,data) 
-  //     let tenantId = getTenantIdForPdf(moduleName,data) || getTenantId()
-  //     debugger
-  //     store.dispatch(showSpinner())
-  //     try{
-  //       let response = await axios.post(`/pdf-service/v1/_createnosave?key=${key}&tenantId=${tenantId}`, body, { // call pdf service to get fileStore id
-  //         'Content-Type': 'application/json',
-  //         'Accept': 'application/json'
-  //        })
-        
-  //        if(response){
-  //          try{
-  //           console.log("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhjdjsjjdfjjdf",response)
-  //          }catch(err){
-
-  //          }
-  //        }
-
-  //     }catch(err){
-  //       if(err){
-  //         try{
-  //           let unsignedFileStoreId = {
-  //             "ResponseInfo":null,
-  //             "fileStoreId":"dfdhgfgjj"
-  //           }
-
-  //           RequestInfo = { ...RequestInfo,"userInfo" :customRequestInfo};
-  //           let body =  Object.assign(
-  //             {},
-  //             {
-  //               RequestInfo,
-  //               "tenantId":getTenantId(),
-  //               "responseData":null,
-  //               "file":unsignedFileStoreId.fileStoreId,
-  //               "fileName":"unsigned.pdf",
-  //               "tokenDisplayName":token,
-  //               "certificate" : certificate,
-  //               "keyId":"CERT_ID",
-  //               "channelId":"default",
-  //               "keyStorePassPhrase": password
-  //             } 
-  //           );
-
-  //           let response = await axios.post("/dsc-services.egov:8080/dsc-services/dsc/_pdfSignInput", body, { // send file store id to get encrypted data
-  //             'Content-Type': 'application/json',
-  //             'Accept': 'application/json'
-  //           })
-  //         }catch(err){
-  //           if(err){
-  //             try{
-  //             let encryptedData = {
-  //               "ResponseInfo":null,
-  //               "input":{
-  //               "encryptedRequest":"XXXX",
-  //               "encryptionKeyId":"YYYYY",
-  //               }
-  //               }
-  //           let body = encryptedData.input
-  //           let response = await axios.post("/dsc-services/emudhra.com:26769/DSC/PKCSBulkSign", body, { // to get response Data
-  //             'Content-Type': 'application/json',
-  //             'Accept': 'application/json'
-  //           })
-  //           }catch(err){
-  //             if(err){
-  //               try{
-  //             let responseData = {
-  //               "responseData": "QuyKW0m6EdBEuTltdWrj2rA5O77bukrGcxlpp4atnn0KoadBXlTZXoEpHp3Q3Qxne1pcmXUSBedS3Ocj3/5Nqjtj6Q1QuqQxo1yMtoOmeGjlilICxaqs9ldgRu8rlbuXrzeip6VMqMCEM+f21+tKf3c4UKWd6/gYOg8rG+37HAVDRAjz21HECLLP2lq3bThBTIPog74D8Lvs4MHXE7D28kd2znHny2v/r3lGnmLxmzYlMiBUlYPnPQa8WSyOROpfXNDnD0/fgiIUuNA82mXC7F7x4VHf+GYj94aldkeSE7MKSqDPRsSp3/4gJ4Y8bHNa",
-  //               "status": 1,
-  //               "errorMessage": null,
-  //               "version": "3.1.0.0",
-  //               "errorCode": null
-  //             } 
-  //             RequestInfo = { ...RequestInfo,"userInfo" :customRequestInfo};
-  //              let body =  Object.assign(
-  //                {},
-  //                 {
-  //                  RequestInfo,
-  //                  "tokenDisplayName":null,
-  //                  "keyStorePassPhrase":null,
-  //                  "keyId":null,
-  //                  "channelId":"default",
-  //                  "file":null,
-  //                  "fileName":"unsigned.pdf",
-  //                  "tenantId":getTenantId(),
-  //                   responseData:responseData.responseData,
-  //                 }
-  //              );
-  //              let reponse = await axios.post("/dsc-services.egov:8080/dsc-services/dsc/_pdfSign", body, { // to get filestoreId for pdf signing
-  //               'Content-Type': 'application/json',
-  //               'Accept': 'application/json'
-  //              })
-  //               }catch(error){
-  //                 debugger
-  //                 let singedFileStoreId = {
-  //                   "ResponseInfo":null,
-  //                   "fileStoreId": "jfdoooooooooooooooooooooooooffff"
-  //                 }
-  //                 data && data.documents.length > 0 && data.documents.push({
-  //                   "fileName":key,
-  //                   "fileStoreId":singedFileStoreId.fileStoreId
-  //                 })
-  //                 debugger
-  //                 console.log("jkffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",data)
-  //                 return data
-  //               }
-  //             }
-  //           }
-  //           }
-  //         }
-  //       }
-  //     } 
-  // }
 
   //actual function
   export const getPdfDetails = async (data,preparedFinalObject,moduleName) => {
@@ -273,32 +156,30 @@ let customRequestInfo = JSON.parse(getUserInfo())
                   })
                   if(singedFileStoreId){
                     data && data.documents.length > 0 && data.documents.push({
-                      "fileName":key,
+                      "fileName": key,
                       "fileStoreId":singedFileStoreId.data.fileStoreId
                     })
                     return data
                   }
                     }catch(error){
                       this.props.hideSpinner();
-                      return data
+                      store.dispatch(toggleSnackbar(true, error && error.message || '', "error"));
                     }
                 }
                 }catch(err){ 
                   this.props.hideSpinner(); 
-                  return data 
             }
           }
         }catch(err){
           this.props.hideSpinner();
-          return data
         }
       }
     }catch(err){
       this.props.hideSpinner();
       store.dispatch(toggleSnackbar(true, err.message, "error"));
-      return data
     }  
   }
+  
 class Footer extends React.Component {
   state = {
     open: false,

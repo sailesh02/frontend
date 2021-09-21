2af5efa6a07edf46c87eca34d13cf7a2bc3b9ced
Ruchira Naik <ruchira.naik@transerve.com>
2021-09-21 11:49:47 +0530

    fixed issue wrong tenand Id comming from citizen

--- web/rainmaker/dev-packages/egov-wns-dev/src/ui-config/screens/specs/wns/applyResource/review-trade.js
+++ web/rainmaker/dev-packages/egov-wns-dev/src/ui-config/screens/specs/wns/applyResource/review-trade.js
@@ -9,7 +9,12 @@ import {
 import { getConnectionDetails } from './../applyResource/task-connectiondetails';
 import { propertyOwnerDetails } from "../applyResource/task-owner-details";
 import { convertEpochToDateAndHandleNA, handlePropertySubUsageType, handleNA } from '../../utils';
-import { getTenantId } from "egov-ui-kit/utils/localStorageUtils";
+import { getQueryArg } from "egov-ui-framework/ui-utils/commons";
+
+const getTenantId = () => {
+  let id = getQueryArg(window.location.href, "tenantId");
+  return id 
+}
 
 const getHeader = label => {
   return {

917696f3a23bec84d249f859064b6a036f4b8c9a
Ruchira Naik <ruchira.naik@transerve.com>
2021-10-09 14:05:54 +0530

    fixed issue digital signature dropdown getting displayed for citizen

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/common/Header/components/NavigationDrawer/commonMenuItems.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/common/Header/components/NavigationDrawer/commonMenuItems.js
@@ -2,7 +2,37 @@ import React from "react";
 import { Icon } from "components";
 import Label from "egov-ui-kit/utils/translationNode";
 
-export const CommonMenuItems = [
+const commonItemsCitizen = [
+  {
+    primaryText: <Label label="CS_HOME_HEADER_PROFILE" />,
+    route: "/user/profile",
+    leftIcon: <Icon action="social" name="person" className="iconClassHover material-icons whiteColor customMenuItem" />,
+    style: {
+      paddingBottom: "3px",
+      paddingTop: "3px",
+    },
+    id: "header-profile",
+    path: "userprofile",
+    renderforcsr: 1,
+    renderforadmin: 1,
+    renderforPGREmp: 1,
+  },
+  {
+    primaryText: <Label label="CORE_COMMON_LOGOUT" />,
+    route: "/logout",
+    leftIcon: <Icon action="action" name="power-settings-new" className="iconClassHover material-icons whiteColor customMenuItem" />,
+    style: {
+      borderBottom: "none",
+      borderLeft: "red",
+    },
+    id: "header-logout",
+    path: "logout",
+    renderforcsr: 1,
+    renderforadmin: 1,
+    renderforPGREmp: 1,
+  }
+]
+const commonItemsEmployee = [
   {
     primaryText: <Label label="CS_HOME_HEADER_PROFILE" />,
     route: "/user/profile",
@@ -45,4 +75,5 @@ export const CommonMenuItems = [
     renderforadmin: 1,
     renderforPGREmp: 1,
   }
-];
+]
+export const CommonMenuItems = process.env.REACT_APP_NAME == "Citizen" ? commonItemsCitizen : commonItemsEmployee

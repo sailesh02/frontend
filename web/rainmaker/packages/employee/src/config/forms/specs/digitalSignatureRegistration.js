2e57b02bb8084dd4d99f8cc0893a19fd898e4cb6
Ruchira Naik <ruchira.naik@transerve.com>
2021-11-11 18:50:26 +0530

    populated certificate dropdown

--- web/rainmaker/packages/employee/src/config/forms/specs/digitalSignatureRegistration.js
+++ web/rainmaker/packages/employee/src/config/forms/specs/digitalSignatureRegistration.js
@@ -1,3 +1,98 @@
+import { toggleSnackbarAndSetText } from "egov-ui-kit/redux/app/actions";
+import { hideSpinner,showSpinner } from "egov-ui-kit/redux/common/actions";
+import { getLocale, getTenantId,getAccessToken, getUserInfo } from "egov-ui-kit/utils/localStorageUtils";
+import axios from 'axios';
+import { setFieldProperty } from "egov-ui-kit/redux/form/actions";
+import store from "ui-redux/store";
+
+let RequestInfo = {}
+
+const getRequestInfo = () => {
+  const authToken = getAccessToken();
+  let RequestInfo = {
+    apiId: "Rainmaker",
+    ver: ".01",
+    // ts: getDateInEpoch(),
+    action: "_search",
+    did: "1",
+    key: "",
+    msgId: `20170310130900|${getLocale()}`,
+    requesterId: "",
+    authToken
+  };
+  return RequestInfo
+}
+
+const getCustomRequestInfo = () => {
+  return JSON.parse(getUserInfo())
+}
+
+const getCertificateList = (token) => {
+  store.dispatch(showSpinner());
+  let requestInfo = getRequestInfo()
+  RequestInfo = { ...requestInfo,"userInfo" : getCustomRequestInfo()}; 
+  let body =  Object.assign(
+    {},
+    {
+      RequestInfo,
+      "tenantId":getTenantId(),
+      "responseData":null,
+      "tokenDisplayName":token
+    }
+  );
+  
+  axios.post("/dsc-services/dsc/_getInputCertificate", body, { // to get R1 R2
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+   })
+    .then(response => {
+      let body = response.data.input
+      axios.post("https://localhost.emudhra.com:26769/DSC/ListCertificate", body, { // to get R1 R2
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       })
+        .then(response => {
+          RequestInfo = { ...requestInfo,"userInfo" : getCustomRequestInfo()};              
+          let body =  Object.assign(
+             {},
+              {
+               RequestInfo,
+               "tenantId":getTenantId(),
+                responseData:response.data.responseData,
+                tokenDisplayName:token
+              }
+           );
+          axios.post("/dsc-services/dsc/_getCertificate", body, { // to get R1 R2
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+           })
+            .then(response => {
+              let requiredCertificateFormat = response && response.data && response.data.certificates &&
+              response.data.certificates.map (certificate => {
+                return {
+                  label : certificate.commonName,
+                  value : certificate.keyId,
+                  name: certificate.commonName,
+                  value: certificate.keyId
+                }
+              }) || []
+              store.dispatch(hideSpinner());
+              store.dispatch(setFieldProperty("digitalSignatureRegistration", "certificate", "dropDownData", requiredCertificateFormat));
+              return requiredCertificateFormat
+            })
+            .catch(error => {
+              store.dispatch(hideSpinner());
+            });
+        })
+        .catch(error => {
+          store.dispatch(hideSpinner());
+        });
+    })
+    .catch(error => {
+      store.dispatch(hideSpinner());
+    });
+}
+
 const formConfig = {
     name: "digitalSignatureRegistration",
     fields: {
@@ -8,9 +103,11 @@ const formConfig = {
         floatingLabelText: "CORE_COMMON_TOKEN_LABEL",
         hintText: "CORE_COMMON_TOKEN_PLACEHOLDER",
         type: "AutocompleteDropdown",
-        updateDependentFields: ({ formKey, field, dispatch }) => {
+        updateDependentFields: async({ formKey, field, dispatch }) => {
           if (field.value && field.value.length > 0) {
-            // dispatch(setFieldProperty(formKey, field, "value", field.value));
+            if(field && field.value){
+              getCertificateList(field.value)
+            }
           }
         },
       },

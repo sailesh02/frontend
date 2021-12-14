567b4da6819d57d8bc2a2ca3f73e73502e002e80
Ruchira Naik <ruchira.naik@transerve.com>
2021-12-14 13:52:26 +0530

    added rest at last

--- web/rainmaker/dev-packages/egov-ui-framework-core/src/ui-atoms/HtmlElements/Href/index.js
+++ web/rainmaker/dev-packages/egov-ui-framework-core/src/ui-atoms/HtmlElements/Href/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Href=(props)=>{
-  const {children, ...rest,onClick}=props;
+  const {children, onClick, ...rest}=props;
   return (
     <a onClick={props.onClick} {...rest}>
       {children}

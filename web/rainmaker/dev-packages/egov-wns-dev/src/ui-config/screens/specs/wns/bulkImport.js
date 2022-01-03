8de3524758e15b7f64be557d41c161f66edb07cc
Ruchira Naik <ruchira.naik@transerve.com>
2022-01-03 16:44:20 +0530

    added meter status dropdown

--- web/rainmaker/dev-packages/egov-wns-dev/src/ui-config/screens/specs/wns/bulkImport.js
+++ web/rainmaker/dev-packages/egov-wns-dev/src/ui-config/screens/specs/wns/bulkImport.js
@@ -4,6 +4,7 @@ import { getQueryArg,getTodaysDateInYMD, getMaxDate } from "egov-ui-framework/ui
 import { handleScreenConfigurationFieldChange as handleField, initScreen, prepareFinalObject, toggleSnackbar, showSpinner } from "egov-ui-framework/ui-redux/screen-configuration/actions";
 import get from "lodash/get";
 import { getMeterReadingDataBulkImport } from "../../../../ui-utils/commons"
+import { getMdmsDataForMeterStatus,APPLICATIONSTATE } from "../../../../ui-utils/commons"
 
 import {
     getCommonCard,
@@ -17,6 +18,88 @@ import {
     getPattern
   } from "egov-ui-framework/ui-config/screens/specs/utils";
   import { getSearchResults, getMdmsDataForAutopopulated, isWorkflowExists } from "../../../../ui-utils/commons"
+
+  const setAutopopulatedvalues = async (state, dispatch,cardIndex) => {
+    let billingFrequency = get(state, "screenConfiguration.preparedFinalObject.billingCycle");
+    let consumptionDetails = {};
+    let date = new Date();
+    let status = get(state, "screenConfiguration.preparedFinalObject.meterMdmsData.['ws-services-calculation'].MeterStatus[0].code");
+    let checkBillingPeriod = await get(state, `screenConfiguration.preparedFinalObject.consumptionDetails[${cardIndex}]`);
+    try {
+        let lastReadingDate = convertEpochToDate(checkBillingPeriod[0].currentReadingDate);
+        let lastDF = new Date();
+        let endDate = ("0" + lastDF.getDate()).slice(-2) + '/' + ("0" + (lastDF.getMonth() + 1)).slice(-2) + '/' + lastDF.getFullYear()
+        consumptionDetails['billingPeriod'] = lastReadingDate + " - " + endDate
+        consumptionDetails['lastReading'] = checkBillingPeriod[0].currentReading
+        consumptionDetails['consumption'] = ''
+        consumptionDetails['lastReadingDate'] = lastReadingDate
+    }catch (e) { 
+        console.log(e);         
+        dispatch(
+            toggleSnackbar(
+                true,
+                {
+                    labelName: "Failed to parse meter reading data.",
+                    labelKey: "ERR_FAILED_TO_PARSE_METER_READING_DATA"
+                },
+                "warning"
+            )
+        );
+        return;
+    }
+
+    // dispatch(
+    //     handleField(
+    //         "meter-reading",
+    //         "components.div.children.meterReadingEditable.children.card.children.cardContent.children.firstContainer.children.billingCont.children.billingPeriod.props",
+    //         "labelName",
+    //         consumptionDetails.billingPeriod
+    //     )
+    // );
+    // dispatch(
+    //     handleField(
+    //         "meter-reading",
+    //         "components.div.children.meterReadingEditable.children.card.children.cardContent.children.thirdContainer.children.secCont.children.billingPeriod.props",
+    //         "labelName",
+    //         consumptionDetails.lastReading
+    //     )
+    // );
+    // dispatch(
+    //     handleField(
+    //         "meter-reading",
+    //         "components.div.children.meterReadingEditable.children.card.children.cardContent.children.lastReadingContainer.children.secCont.children.billingPeriod.props",
+    //         "labelName",
+    //         consumptionDetails.lastReadingDate
+    //     )
+    // );
+    // dispatch(
+    //     handleField(
+    //         "meter-reading",
+    //         "components.div.children.meterReadingEditable.children.card.children.cardContent.children.sixthContainer.children.secCont.children.billingPeriod.props",
+    //         "labelName",
+    //         consumptionDetails.consumption
+    //     )
+    // );
+    // dispatch(
+    //     handleField(
+    //         "meter-reading",
+    //         "components.div.children.meterReadingEditable.children.card.children.cardContent.children.secondContainer.children.status.props",
+    //         "value",
+    //         status
+    //     )
+    // );
+    // let todayDate = new Date()
+    // dispatch(
+    //     handleField(
+    //         "meter-reading",
+    //         "components.div.children.meterReadingEditable.children.card.children.cardContent.children.fifthContainer.children.currentReadingDate.props",
+    //         "value",
+    //         todayDate
+    //     )
+    // );
+    dispatch(prepareFinalObject(`autoPopulatedValues[${index}]`, consumptionDetails));
+
+}
 
   const addMeterReading = async(state,dispatch,cartIndex,tenantId,connectionNo) => {
     dispatch(showSpinner());
@@ -74,9 +157,7 @@ import {
             return;
         } else {
             await getMdmsDataForAutopopulated(dispatch)
-            // await getMdmsDataForMeterStatus(dispatch)
-            // await setAutopopulatedvalues(state, dispatch)
-            showHideCard(true, dispatch); 
+            await setAutopopulatedvalues(state, dispatch,cardIndex)
         }
 
     }  
@@ -129,11 +210,17 @@ const header = getCommonHeader(
   }
 );
 
+const getData = async (action, state, dispatch) => {
+  await getMdmsDataForMeterStatus(dispatch)
+
+}
+
 const screenConfig = {
   uiFramework: "material-ui",
   name: "bulkImport",
   beforeInitScreen: (action, state, dispatch) => {
-    // fetchData(action, state, dispatch);
+    getData(action, state, dispatch).then(() => {
+    });
     return action;
   },
   // components: {
@@ -234,31 +321,173 @@ const screenConfig = {
                     disabled:true,
                     jsonPath: "meterReadings[0].billingPeriod"
                   }),
-                  meterStatus: getSelectField({
-                    label: {
-                      labelName: "WS_METER_STATUS",
-                      labelKey: "WS_METER_STATUS"
-                    },
-                    placeholder: {
-                      labelName: "WS_METER_STATUS_PLACEHOLDER",
-                      labelKey: "WS_METER_STATUS_PLACEHOLDER"
-                    },
-                    required: true,
-                    visible: true,
-                    jsonPath: "meterReadings[0].meterStatus",
-                    gridDefination: {
-                      xs: 12,
-                      sm: 3
-                    },
-                    data: [
-                      {
-                        code: "working"
-                      },
-                      {
-                        code: "working"
-                      }
-                    ]
-                  }),
+                  meterStatus:
+                  {
+                      ...getSelectField({
+                          placeholder: {
+                              labelKey: "WS_SELECT_METER_STATUS_PLACEHOLDER"
+                          },
+                          labelPrefix: {
+                              moduleName: "ws-services-calculation",
+                              masterName: "MeterStatus"
+                          },
+                          props: {
+                              value: "",
+                          },
+                          sourceJsonPath: "meterMdmsData['ws-services-calculation'].MeterStatus",
+                          jsonPath: "meterReadings[0].meterStatus",
+                          gridDefination: {
+                              xs: 6,
+                              sm: 3
+                          },
+                          required: false,
+                          errorMessage: "ERR_INVALID_BILLING_PERIOD",
+                      }),
+                      // afterFieldChange: async (action, state, dispatch) => {
+                      //     let status = get(state, "screenConfiguration.preparedFinalObject.metereading.meterStatus");
+                      //     if (status !== 'Working') {
+                      //         dispatch(
+                      //             handleField(
+                      //                 "meter-reading",
+                      //                 "components.div.children.meterReadingEditable.children.card.children.cardContent.children.fourthContainer.children.currentReading.props",
+                      //                 "disabled",
+                      //                 true
+                      //             )
+                      //         );
+                      //         dispatch(
+                      //             handleField(
+                      //                 "meter-reading",
+                      //                 "components.div.children.meterReadingEditable.children.card.children.cardContent.children.fifthContainer.children.currentReadingDate.props",
+                      //                 "disabled",
+                      //                 true
+                      //             )
+                      //         );
+                      //         dispatch(
+                      //             handleField(
+                      //                 "meter-reading",
+                      //                 "components.div.children.meterReadingEditable.children.card.children.cardContent.children.sixthContainer.children.secCont",
+                      //                 "visible",
+                      //                 false
+                      //             )
+                      //         );
+                      //         dispatch(
+                      //             handleField(
+                      //                 "meter-reading",
+                      //                 "components.div.children.meterReadingEditable.children.card.children.cardContent.children.sixthContainer.children.thirdCont",
+                      //                 "visible",
+                      //                 true
+                      //             )
+                      //         );
+                      //         dispatch(
+                      //             handleField(
+                      //                 "meter-reading",
+                      //                 "components.div.children.meterReadingEditable.children.card.children.cardContent.children.sixthContainer.children.thirdCont",
+                      //                 "visible",
+                      //                 true
+                      //             )
+                      //         );
+                      //         dispatch(
+                      //             handleField(
+                      //                 "meter-reading",
+                      //                 "components.div.children.meterReadingEditable.children.card.children.cardContent.children.fifthContainer.children.currentReadingDate.props",
+                      //                 "value",
+                      //                 ""
+                      //             )
+                      //         );
+                      //         dispatch(
+                      //             handleField(
+                      //                 "meter-reading",
+                      //                 "components.div.children.meterReadingEditable.children.card.children.cardContent.children.fourthContainer.children.currentReading.props",
+                      //                 "value",
+                      //                 ""
+                      //             )
+                      //         );
+                      //         dispatch(
+                      //             handleField(
+                      //                 "meter-reading",
+                      //                 "components.div.children.meterReadingEditable.children.card.children.cardContent.children.sixthContainer.children.secCont.children.billingPeriod.props",
+                      //                 "labelName",
+                      //                 ""
+                      //             )
+                      //         );
+                      //         dispatch(
+                      //             handleField(
+                      //                 "meter-reading",
+                      //                 "components.div.children.meterReadingEditable.children.card.children.cardContent.children.sixthContainer.children.thirdCont.props",
+                      //                 "value",
+                      //                 ""
+                      //             )
+                      //         );
+                      //     } else {
+                      //         dispatch(
+                      //             handleField(
+                      //                 "meter-reading",
+                      //                 "components.div.children.meterReadingEditable.children.card.children.cardContent.children.fourthContainer.children.currentReading.props",
+                      //                 "disabled",
+                      //                 false
+                      //             )
+                      //         );
+                      //         dispatch(
+                      //             handleField(
+                      //                 "meter-reading",
+                      //                 "components.div.children.meterReadingEditable.children.card.children.cardContent.children.fifthContainer.children.currentReadingDate.props",
+                      //                 "disabled",
+                      //                 false
+                      //             )
+                      //         );
+                      //         dispatch(
+                      //             handleField(
+                      //                 "meter-reading",
+                      //                 "components.div.children.meterReadingEditable.children.card.children.cardContent.children.sixthContainer.children.secCont",
+                      //                 "visible",
+                      //                 true
+                      //             )
+                      //         );
+                      //         dispatch(
+                      //             handleField(
+                      //                 "meter-reading",
+                      //                 "components.div.children.meterReadingEditable.children.card.children.cardContent.children.sixthContainer.children.thirdCont",
+                      //                 "visible",
+                      //                 false
+                      //             )
+                      //         );
+                      //         let todayDate = new Date()
+                      //         dispatch(
+                      //             handleField(
+                      //                 "meter-reading",
+                      //                 "components.div.children.meterReadingEditable.children.card.children.cardContent.children.fifthContainer.children.currentReadingDate.props",
+                      //                 "value",
+                      //                 todayDate
+                      //             )
+                      //         );
+                      //         dispatch(
+                      //             handleField(
+                      //                 "meter-reading",
+                      //                 "components.div.children.meterReadingEditable.children.card.children.cardContent.children.fourthContainer.children.currentReading.props",
+                      //                 "value",
+                      //                 ""
+                      //             )
+                      //         );
+                      //         dispatch(
+                      //             handleField(
+                      //                 "meter-reading",
+                      //                 "components.div.children.meterReadingEditable.children.card.children.cardContent.children.sixthContainer.children.thirdCont.props",
+                      //                 "value",
+                      //                 ""
+                      //             )
+                      //         );
+                      //         dispatch(
+                      //             handleField(
+                      //                 "meter-reading",
+                      //                 "components.div.children.meterReadingEditable.children.card.children.cardContent.children.sixthContainer.children.secCont.children.billingPeriod.props",
+                      //                 "labelName",
+                      //                 ""
+                      //             )
+                      //         );
+                      //     }
+  
+                      // }
+                  },
                   lastReadingDate: {
                     ...getDateField({
                       label: {

112130f10f352fe43543ead77ef3bc36c6accc57
neeraj <kumar.neeraj@velocis.co.in>
2022-07-02 14:06:20 +0530

    fix: allow number and alphabets in applicant name while doing scrutiny

--- web/rainmaker/dev-packages/egov-bpa-dev/src/ui-config/screens/specs/edcrscrutiny/apply.js
+++ web/rainmaker/dev-packages/egov-bpa-dev/src/ui-config/screens/specs/edcrscrutiny/apply.js
@@ -168,7 +168,7 @@ const buildingInfoCard = getCommonCard({
           sm: 6
         },
         required: true,
-        pattern: getPattern("Name"),
+        pattern: getPattern("Address"),
         jsonPath: "Scrutiny[0].applicantName"
       }),
       dummyDiv1: {
